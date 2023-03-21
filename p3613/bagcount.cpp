0b6f64977ce00d123c5a1d30def9172258a6d9d0
HongqingXi <xiacrossthesea@gmail.com>
2023-03-21 09:51:32 +0800

    bag

--- p3613/bagcount.cpp
+++ p3613/bagcount.cpp
@@ -2,7 +2,7 @@
  * @Author: HongqingXi xiacrossthesea@gmail.com
  * @Date: 2023-03-20 09:45:08
  * @LastEditors: HongqingXi xiacrossthesea@gmail.com
- * @LastEditTime: 2023-03-20 10:32:48
+ * @LastEditTime: 2023-03-21 09:46:36
  * @FilePath: /CProject/p3613/bagcount.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,7 +12,7 @@ using namespace std;
 int main(){
     //指针数组可以做
     int n,q;
-    int sign;
+    int sign,flag;
     cin>>n>>q;
     int a[q][3];
     int l=0;
@@ -29,21 +29,25 @@ int main(){
         if(sign==2){
             int x,y,k;
             cin>>x>>y;
+            flag=0;
             for(k=0;k<j;k++){
                 if((a[k][0]==x)&&(a[k][1]==y)){
                     m[l++]=a[k][2];
+                    flag=1;
                 }
+            }
+            if(flag==0){
+                m[l++]=0;
             }
         }
     }
     for(int i=0;i<l;i++){
         if(i!=0){
-            if(m[i]!=0)cout<<endl<<m[i];
+            cout<<endl<<m[i];
         }
         else{
-            if(m[i]!=0)cout<<m[i];            
+            cout<<m[i];            
         }
-
     }
     
     return 0;
